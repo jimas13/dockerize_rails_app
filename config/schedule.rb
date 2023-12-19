f6bf9e1de082a6770d753ba1ff6c7f683a0c15e5
jimas13 <machetecontrolande@hotmail.com>
2023-12-19 22:43:03 +0000

    setup sample job and job schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,4 +17,8 @@
 #   runner "AnotherModel.prune_old_records"
 # end
 
+every 1.minute do
+  rake 'sample:test'
+end
+
 # Learn more: http://github.com/javan/whenever
---
+++ lib/tasks/sample.rake
@@ -0,0 +1,8 @@
+namespace :sample do
+  desc 'saying hi to cron'
+    task :test => [ :environment ] do
+       puts 'hi cron :)'
+    end
+
+
+end
