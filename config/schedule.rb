d32b2998b35f70a377de5461911eb422d7fc07b6
jimas13 <machetecontrolande@hotmail.com>
2023-12-19 22:43:39 +0000

    set up scheduled task

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,10 @@
 # Example:
 #
 # set :output, "/path/to/my/cron_log.log"
-#
+
+#custom logs location
+#set :output, "log/cron.log"
+
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
 #   runner "MyModel.some_method"
